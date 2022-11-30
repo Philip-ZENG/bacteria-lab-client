955d1a4ff5fe3c73bdd26d6cb27b4f878b1c7627
ThomasXU-17 <xuanyangxu@link.cuhk.edu.cn>
2022-11-30 15:39:48 +0800

    gameWorld 1st

--- pages/gameWorld.js
+++ pages/gameWorld.js
@@ -3,15 +3,122 @@
 import React, {Component} from 'react';
 import web3 from '../eth/web3';
 import bacteriaLabCore from '../eth/bacteriaLabCore';
+import 'semantic-ui-css/semantic.min.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
+import PlayerInfo from '../component/PlayerInfo';
+import Gameboard from '../component/Gameboard';
+
+import {
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Header,
+  Image,
+  List,
+  Menu,
+  Segment,
+} from 'semantic-ui-react'
 
 class GameWorld extends Component {
 
   render() {
     return (
-      <div>Welcome to Bacteria Lab Game World!</div>
+    <div>
+      <Menu inverted>
+          <Menu.Item as='a' header>
+            <Image size='mini' src='/icon.png' style={{ marginRight: '0em' }} />
+              Bacteria Lab
+          </Menu.Item>
+          <Menu.Item as='a'>Home</Menu.Item>
+
+          <Dropdown item simple text='Dropdown'>
+            <Dropdown.Menu>
+              <Dropdown.Item>List Item</Dropdown.Item>
+              <Dropdown.Item>List Item</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Header>Header Item</Dropdown.Header>
+              <Dropdown.Item>
+                <i className='dropdown icon' />
+                <span className='text'>Submenu</span>
+                <Dropdown.Menu>
+                  <Dropdown.Item>List Item</Dropdown.Item>
+                  <Dropdown.Item>List Item</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown.Item>
+              <Dropdown.Item>List Item</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+      </Menu>
+
+
+        <Grid columns={4} divided>
+          <Grid.Row>
+            <Grid.Column width={1}>
+            </Grid.Column>
+            <Grid.Column width={7}>
+              <div style={{height: '25px'}}>
+              </div>
+              <Container textAlign='center'>
+                <Gameboard />
+              </Container>
+            </Grid.Column>
+
+            <Grid.Column width={7}>
+              <div style={{height: '150px'}}>
+                <PlayerInfo />
+              </div>
+              <div style={{height: '200px'}}>
+                Tile Info
+              </div>
+              <div style={{height: '150px'}}>
+                Action
+              </div>
+            </Grid.Column>
+            <Grid.Column width={1}>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+
+      <Segment inverted vertical style={{ margin: '2em 0em 0em', padding: '1.5em 0em' }}>
+        <Container textAlign='center'>
+          <Grid divided inverted stackable>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Xuanyang Xu' />
+              <List link inverted>
+                <List.Item as='a'>119010361</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Zhuoru Zeng' />
+              <List link inverted>
+                <List.Item as='a'>Link One</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Yan Zhuang' />
+              <List link inverted>
+                <List.Item as='a'>Link One</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={7}>
+              <Header inverted as='h4' content='FTE4312 Project: Bacteria Lab' />
+              <List link inverted>
+                <List.Item as='a'>Link to Github</List.Item>
+              </List>
+            </Grid.Column>
+          </Grid>
+        </Container>
+      </Segment>        
+    </div>
+
+
     );
   };
 }
 
 export default GameWorld
+
+
