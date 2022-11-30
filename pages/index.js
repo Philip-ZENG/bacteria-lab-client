67a6cc1c724b6a8767b1a9abcd233cea9af6c3ac
ThomasXU-17 <xuanyangxu@link.cuhk.edu.cn>
2022-11-30 21:55:09 +0800

    index draft

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,20 @@
 import React, {Component} from 'react';
 import web3 from '../eth/web3';
 import invitePlayer from '../eth/invitePlayer';
+import 'semantic-ui-css/semantic.min.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import {
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Header,
+  Image,
+  List,
+  Menu,
+  Segment,
+} from 'semantic-ui-react'
 
 class InvitePlayer extends Component {
   state = {
@@ -47,24 +61,108 @@ class InvitePlayer extends Component {
 
     return (
       <div>
-        <h1>Invite Player into the BacteriaLab Game</h1>
-        <p>The Admin Address of this game is: {this.state.adminAddress}</p>
-        <p>Currently {this.state.playerCount} players have join the game</p>
-        <p>The address of the last player who join the game is shwon below:</p>
-        <p>{this.state.lastPlayer}</p>
-        <form onSubmit={this.onSubmit}>
-          <h4>Invite New Player</h4>
-          <div>
-            <label>Enter Address of New Player</label>
-            <input 
-              value={this.state.newPlayerAddress}
-              onChange={event => this.setState({ newPlayerAddress: event.target.value })}
-            />
-          </div>
-          <button>Invite</button>
-        </form>
-  
-        <h1>{this.state.message}</h1>
+        <Menu inverted>
+          <Menu.Item as='a' header>
+            <Image size='mini' src='/icon.png' style={{ marginRight: '0em' }} />
+              Bacteria Lab
+          </Menu.Item>
+          <Menu.Item as='a'>Home</Menu.Item>
+
+          <Dropdown item simple text='Dropdown'>
+            <Dropdown.Menu>
+              <Dropdown.Item>List Item</Dropdown.Item>
+              <Dropdown.Item>List Item</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Header>Header Item</Dropdown.Header>
+              <Dropdown.Item>
+                <i className='dropdown icon' />
+                <span className='text'>Submenu</span>
+                <Dropdown.Menu>
+                  <Dropdown.Item>List Item</Dropdown.Item>
+                  <Dropdown.Item>List Item</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown.Item>
+              <Dropdown.Item>List Item</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu>
+        <Grid columns={4} divided>
+          <style>
+            {`
+            html, body {background-color: #252839 !important;}
+            `}
+          </style>
+          
+          <Grid.Row>
+            <Grid.Column width={1}>
+              
+            </Grid.Column>
+
+            <Grid.Column width={7}>
+              <h1>Invite Player into the BacteriaLab Game</h1>
+              <p>The Admin Address of this game is: {this.state.adminAddress}</p>
+              <p>Currently {this.state.playerCount} players have join the game</p>
+              <p>The address of the last player who join the game is shwon below:</p>
+              <p>{this.state.lastPlayer}</p>
+              <form onSubmit={this.onSubmit}>
+                <h4>Invite New Player</h4>
+                <div>
+                  <label>Enter Address of New Player</label>
+                  <input 
+                    value={this.state.newPlayerAddress}
+                    onChange={event => this.setState({ newPlayerAddress: event.target.value })}
+                  />
+                </div>
+                <button>Invite</button>
+              </form>
+              <h1>{this.state.message}</h1>
+            </Grid.Column>
+
+            <Grid.Column width={7}>
+              <Container textAlign='center'>
+              <div style={{height: '72px'}}>
+              </div>
+                <Image size='big' src='/icon.png' verticalAlign='middle'/>
+              <div style={{height: '73px'}}>
+              </div>
+              </Container>
+            </Grid.Column>
+
+            <Grid.Column width={1}>
+              
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Segment inverted vertical style={{ margin: '2em 0em 0em', padding: '1.5em 0em' }}>
+        <Container textAlign='center'>
+          <Grid divided inverted stackable>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Xuanyang Xu' />
+              <List link inverted>
+                <List.Item as='a'>119010361</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Zhuoru Zeng' />
+              <List link inverted>
+                <List.Item as='a'>Link One</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Yan Zhuang' />
+              <List link inverted>
+                <List.Item as='a'>Link One</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={7}>
+              <Header inverted as='h4' content='FTE4312 Project: Bacteria Lab' />
+              <List link inverted>
+                <List.Item as='a'>Link to Github</List.Item>
+              </List>
+            </Grid.Column>
+          </Grid>
+        </Container>
+        </Segment>
       </div>
 
     );
