0cec8b95543bddc2dc633df263585bea8d023864
Philip-ZENG <892542757@qq.com>
2022-12-01 23:17:12 +0800

    configure enterGame function

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "150f5c071f947dc5"; }
+/******/ 		__webpack_require__.h = function() { return "323790f7d02d957a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
